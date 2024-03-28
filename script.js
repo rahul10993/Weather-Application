c1ca7802b62345032ce7c7a26e4b5364bf5a66a8
rahul10993 <123195630+rahul10993@users.noreply.github.com>
2024-03-28 12:18:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// https://api.openweathermap.org/data/2.5/weather?q=Agartala&appid=41ca8090034a5aecfefc882bc32782f2&units=metric
+//
+//
+const apiKey = "41ca8090034a5aecfefc882bc32782f2";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  if (response.status === 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    let data = await response.json();
+    //   console.log(data);
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".Wind").innerHTML = data.wind.speed + " Km/h";
+    if (data.weather[0].main === "Clouds") {
+      weatherIcon.src = "./weather-app-img/images/clouds.png";
+    } else if (data.weather[0].main === "Clear") {
+      weatherIcon.src = "./weather-app-img/images/clear.png";
+    } else if (data.weather[0].main === "Rain") {
+      weatherIcon.src = "./weather-app-img/images/rain.png";
+    } else if (data.weather[0].main === "Drizzle") {
+      weatherIcon.src = "./weather-app-img/images/drizzle.png";
+    } else if (data.weather[0].main === "Mist") {
+      weatherIcon.src = "./weather-app-img/images/mist.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  //   console.log("is clicked");
+  checkWeather(searchBox.value);
+});
+
+// checkWeather();
